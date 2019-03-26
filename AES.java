cc253aafeca981e4ad9169614236a1f9fe7ade5d
Zojae031 <edgar3@naver.com>
2019-03-26 14:10:05 +0900

    AES256/Base64 암,복호화 코드

---
+++ AES.java
@@ -0,0 +1,66 @@
+﻿import com.sun.istack.internal.NotNull;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.AlgorithmParameterSpec;
+import java.util.Base64;
+
+public class AES {
+    public static byte[] ivBytes = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}; //암호화 복호화 동일해야함.
+
+    public static String secretKey = "capstoneDesign12"; //16bytes 를 맞추어야 한다.
+
+    public static void main(String arg[]) {
+
+
+        try {
+             String encode = AES_Encode("14011038");
+             String decode = AES_Decode(encode);
+            System.out.println("암호화된 데이터 : "+encode);
+            System.out.println("복호화된 데이터 : "+decode);
+        }  catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (NoSuchPaddingException e) {
+            e.printStackTrace();
+        } catch (InvalidAlgorithmParameterException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (BadPaddingException e) {
+            e.printStackTrace();
+        } catch (IllegalBlockSizeException e) {
+            e.printStackTrace();
+        }
+
+    }
+    //암호화
+    public static String AES_Encode(String str) throws  NoSuchAlgorithmException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] textByte = str.getBytes(StandardCharsets.UTF_8);
+        AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+        SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8), "AES");
+        @NotNull
+        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.ENCRYPT_MODE, newKey, ivSpec);
+        return Base64.getEncoder().encodeToString(cipher.doFinal(textByte));
+    }
+
+    //복호화
+    public static String AES_Decode(String str) throws  NoSuchPaddingException, NoSuchAlgorithmException, InvalidAlgorithmParameterException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
+        byte[] textBytes = Base64.getDecoder().decode(str);
+        AlgorithmParameterSpec ivSpec = new IvParameterSpec(ivBytes);
+        SecretKeySpec newKey = new SecretKeySpec(secretKey.getBytes(StandardCharsets.UTF_8),"AES");
+        final Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, newKey, ivSpec);
+        return new String(cipher.doFinal(textBytes), StandardCharsets.UTF_8);
+    }
+
+
+}
